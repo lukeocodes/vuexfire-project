8c7f73926d1ab773bbed4998dd707958da870e17
@lukeocodes <luke@lukeoliff.com>
2021-03-09 16:41:57 +0000

    feat(firestore): add update document from todo list

--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,14 @@ export default new Vuex.Store({
     ...vuexfireMutations,
   },
   actions: {
+    updateTodo: firestoreAction(( context , data) => {
+      db.collection('todos')
+        .doc(data.id)
+        .update({ message: data.message })
+        .then(() => {
+          console.log('todo updated!')
+        })
+    }),
     bindTodos: firestoreAction(({ bindFirestoreRef }) => {
       // return the promise returned by `bindFirestoreRef`
       return bindFirestoreRef('todos', db.collection('todos'))
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is a To-do page</h1>
     <ul>
-      <li v-for="(todo, index) in todos" :key="index">{{ todo.message }}</li>
+      <li v-for="(todo, index) in todos" :key="index" @click="updateTodo(todo.id)">{{ todo.message }}</li>
     </ul>
   </div>
 </template>
@@ -10,16 +10,22 @@
 <script>
 export default {
   destroy() {
-    this.$store.dispatch('unbindTodos')
+    this.$store.dispatch("unbindTodos")
   },
 
   mounted() {
-    this.$store.dispatch('bindTodos')
+    this.$store.dispatch("bindTodos")
   },
 
   computed: {
     todos() {
       return this.$store.state.todos
+    }
+  },
+
+  methods: {
+    updateTodo(id) {
+      this.$store.dispatch("updateTodo", { id: id, message: 'I GOT UPDATE, YO!'})
     }
   }
 }
