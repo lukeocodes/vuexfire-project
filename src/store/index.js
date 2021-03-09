e1209abf73f3e83f465f6641153189a07646de08
@lukeocodes <luke@lukeoliff.com>
2021-03-09 16:14:01 +0000

    feat(firestore): initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { vuexfireMutations, firestoreAction } from 'vuexfire'
+import { db } from '../db'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: [],
   },
   mutations: {
+    ...vuexfireMutations,
   },
   actions: {
+    bindTodos: firestoreAction(({ bindFirestoreRef }) => {
+      // return the promise returned by `bindFirestoreRef`
+      return bindFirestoreRef('todos', db.collection('todos'))
+    }),
   },
   modules: {
   }
---
+++ src/views/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is a To-do page</h1>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">{{ todo.message }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$store.dispatch('bindTodos')
+  },
+
+  computed: {
+    todos() {
+      return this.$store.state.todos
+    }
+  }
+}
+</script>
+
